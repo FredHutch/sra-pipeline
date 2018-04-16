ec9a64fbb1ac721ab9b19e894d1484d7ac0f17c2
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-04-16 14:31:45 -0700

    add utility for monitoring/submitting jobs, & a README

---
+++ README.md
@@ -0,0 +1,86 @@
+# SRA Pipeline
+
+This repository contains code for running an analysis pipeline in
+[AWS Batch](https://aws.amazon.com/batch/).
+
+## What the pipeline does
+
+Given a set of SRA accession numbers, AWS Batch will start an
+[array job](https://docs.aws.amazon.com/batch/latest/userguide/array_jobs.html)
+where each child will process a single accession number, doing the following:
+
+* Download the file(s) associated with the accession number from
+  [SRA](https://www.ncbi.nlm.nih.gov/sra), using the
+  [prefetch](https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=toolkit_doc&f=prefetch)
+  tool with the [Aspera Connect](http://downloads.asperasoft.com/connect2//) transport.
+* Start a [bash](https://en.wikipedia.org/wiki/Bash_(Unix_shell)) pipe which
+  runs the following steps, once for each of three viral genomes.
+  * extracts the downloaded `.sra` file to `fastq` format using
+    [fastq-dump](https://ncbi.github.io/sra-tools/fastq-dump.html). The sra
+    file is highly compressed and this step can expand it to more than 20 times
+    its size, which is one reason we stream the data in a pipe: so as to
+    not need lots of scratch space.
+ * Pipe the `fastq` data through\
+   [bowtie2](http://bowtie-bio.sourceforge.net/bowtie2/index.shtml)
+   to search for the virus.
+ * Pipe the output of `bowtie2` through
+   [gzip](https://en.wikipedia.org/wiki/Gzip) to compress it prior to
+   the next step.
+ * stream the compressed output of `bowtie2` to an
+   [S3](https://aws.amazon.com/s3/) bucket. The resulting file will
+   have an S3 URL like this: `s3://<bucket-name>/pipeline-results/<SRA-accession-number>/<virus>/<SRA-accession-number>.sam.gz`.
+
+
+## Prerequisites/Requirements
+
+* You must run all these tools on the Fred Hutch internal network.
+* You must obtain your S3 credentials using the
+  [awscreds](https://teams.fhcrc.org/sites/citwiki/SciComp/Pages/Getting%20AWS%20Credentials.aspx)
+  script. You only need to do this once.
+* You must clone this repository to a location under your home directory, and then
+  change directories into the repository (you only need to do this once,
+  although you may need to run `git pull` periodically to keep
+  your cloned repository up to date):
+
+```
+git clone https://github.com/FredHutch/sra-pipeline.git
+cd sra-pipeline
+```
+
+
+
+## `sra_pipeline` utility
+
+A script called `sra_pipeline` is available to to simplify the following:
+
+* Display accession numbers that have already been processed.
+* Display accession numbers which are currently being processed.
+* Submit some number of new accession numbers to the pipeline, choosing
+  either randomly, or by picking the smallest available data sets.
+
+Running the utility with `--help` gives usage information:
+
+```
+$ ./sra_pipeline --help
+usage: sra_pipeline.py [-h] [-c] [-i] [-s N] [-r N]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c, --completed       show completed accession numbers
+  -i, --in-progress     show accession numbers that are in progress
+  -s N, --submit-small N
+                        submit N jobs of ascending size
+  -r N, --submit-random N
+                        submit N randomly chosen jobs
+```
+
+
+## Additional monitoring of jobs
+
+You can get more detail about running jobs by using  
+the [Batch Dashboard](https://batch-dashboard.fhcrc.org/)
+and/or the
+[AWS command-line client for Batch](https://docs.aws.amazon.com/cli/latest/reference/batch/index.html).
+
+See [Using AWS Batch at Fred Hutch](https://fredhutch.github.io/aws-batch-at-hutch-docs/)
+for more information.
---
+++ sra_pipeline.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+
+"""
+Utility for working with SRA pipeline jobs.
+"""
+
+import argparse
+import datetime
+import io
+import json
+import os
+import sys
+
+from collections import defaultdict
+from math import ceil
+from urllib.parse import urlparse
+
+import boto3
+import numpy as np
+import pandas as pd
+
+def show_completed():
+    "show completed accession numbers"
+    s3 = boto3.client("s3") # pylint: disable=invalid-name
+    completed_map = defaultdict(list)
+    args = dict(Bucket="fh-pi-jerome-k", Prefix="pipeline-results", MaxKeys=3)
+    while True:
+        response = s3.list_objects_v2(**args)
+        for item in response['Contents']:
+            segs = item['Key'].split("/")
+            accession = segs[1]
+            virus = segs[2]
+            completed_map[accession].append(virus)
+        try:
+            args['ContinuationToken'] = response['NextContinuationToken']
+        except KeyError:
+            break
+    completed = [x for x in completed_map.keys() if len(completed_map[x]) == 3]
+    return completed
+
+
+def show_in_progress(): # pylint: disable=too-many-locals
+    "show accession numbers that are in progress"
+    s3 = boto3.client("s3") # pylint: disable=invalid-name
+    batch = boto3.client("batch")
+    in_progress_states = ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING', 'RUNNING']
+    state_jobs = []
+    for state in in_progress_states:
+        results = batch.list_jobs(jobQueue="mixed", jobStatus=state)
+        state_jobs.extend(results['jobSummaryList'])
+    job_ids = [x['jobId'] for x in state_jobs]
+    if not len(job_ids):
+        return []
+    chunks = []
+    jobs = []
+    if len(job_ids) <= 100:
+        chunks.append(job_ids)
+    else:
+        num_chunks = int(ceil(len(job_ids) / 100.0))
+        chunks = np.array_split(job_ids, num_chunks)
+    for chunk in chunks:
+        response = batch.describe_jobs(jobs=chunk)
+        jobs.extend(response['jobs'])
+    accession_lists_map = {}
+    for job in jobs:
+        if 'container' in job and 'environment' in job['container']:
+            for item in job['container']['environment']:
+                if item['name'] == 'ACCESSION_LIST':
+                    accession_lists_map[item['value']] = 1
+    accession_nums = []
+    for item in accession_lists_map:
+        url = urlparse(item)
+        bucket = url.netloc
+        key = url.path.lstrip("/")
+        flh = io.BytesIO()
+        s3.download_fileobj(bucket, key, flh)
+        accession_nums.extend(flh.getvalue().decode('utf-8').strip().split("\n"))
+    return accession_nums
+
+
+
+def select_from_csv(num_rows, method):
+    """
+    Selects accession numbers from the csv file.
+    Args:
+        num_rows (int): the number of accession numbers to return.
+                        Will return all available rows if this number
+                        is larger than the number of rows.
+        method (str): one of "random" or "small". "random" selects accession
+                      numbers randomly; "small"  selects them by size
+                      (in ascending order).
+    """
+    if not method in ['small', 'random']:
+        raise ValueError("invalid method! must be 'small' or 'random'")
+    raw_df = pd.read_csv("srr-sizes.csv")
+    exclude = []
+    exclude.extend(show_completed())
+    exclude.extend(show_in_progress())
+    df = raw_df[~raw_df['accession_number'].isin(exclude)] # pylint: disable=invalid-name
+    nrow = df.shape[0]
+    if num_rows > nrow:
+        num_rows = nrow
+    if method == "small":
+        return df['accession_number'].head(num_rows).tolist()
+    return df['accession_number'].sample(num_rows).tolist()
+
+def to_aws_env(env):
+    "convert dict to name/value pairs"
+    out = []
+    for key, val in env.items():
+        out.append(dict(name=key, value=val))
+    return out
+
+
+def get_latest_jobdef_revision(batch_client, jobdef_name): # FIXME handle pagination
+    "get the most recent revision for a job definition"
+    results = \
+      batch_client.describe_job_definitions(status="ACTIVE",
+                                            jobDefinitionName=jobdef_name)['jobDefinitions']
+    if not results:
+        raise ValueError("No job definition called {}.".format(jobdef_name))
+    return max(results, key=lambda x: x['revision'])['revision']
+
+def submit(num_rows, method): # pylint: disable=too-many-locals
+    """
+    Utility function to submit jobs.
+    Args:
+        num_rows: Number of accession numbers to process, passed to select_from_csv()
+        method: Either 'random' or 'small', passed to select_from_csv()
+    """
+    s3 = boto3.client("s3") # pylint: disable=invalid-name
+    batch = boto3.client("batch")
+    now = datetime.datetime.now()
+    nowstr = now.strftime("%Y%m%d%H%M%S")
+    accession_nums = select_from_csv(num_rows, method)
+    bytesarr = bytearray("\n".join(accession_nums), "utf-8")
+    bytesio = io.BytesIO(bytesarr)
+    job_size = len(accession_nums)
+    key = "{}-{}.txt".format(nowstr, job_size)
+    url = "s3://fh-pi-jerome-k/sra-submission-manifests/{}".format(key)
+    s3.upload_fileobj(bytesio, "fh-pi-jerome-k", "sra-submission-manifests/{}".format(key))
+    job_name = "sra-pipeline-{}-{}-{}".format(os.getenv("USER"), nowstr, job_size)
+    env = to_aws_env(dict(BUCKET_NAME="fh-pi-jerome-k", PREFIX="pipeline-results",
+                          ACCESSION_LIST=url))
+    job_def_name = "sra-pipeline" # use "hello" for testing, "sra-pipeline" for production
+    jobdef = "{}:{}".format(job_def_name, get_latest_jobdef_revision(batch, job_def_name))
+    res = batch.submit_job(jobName=job_name, jobQueue="mixed",
+                           arrayProperties=dict(size=job_size),
+                           jobDefinition=jobdef,
+                           containerOverrides=dict(environment=env))
+
+    del res['ResponseMetadata']
+    return res
+
+def submit_small(num_jobs):
+    "submit <num_jobs> jobs of ascending size"
+    return submit(num_jobs, "small")
+
+def submit_random(num_jobs):
+    "submit <num_jobs> randomly chosen jobs"
+    return submit(num_jobs, "random")
+
+def main():
+    "do the work"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--completed", help="show completed accession numbers",
+                        action="store_true")
+    parser.add_argument("-i", "--in-progress",
+                        help="show accession numbers that are in progress",
+                        action="store_true")
+    parser.add_argument("-s", "--submit-small", help="submit N jobs of ascending size",
+                        type=int, metavar='N')
+    parser.add_argument("-r", "--submit-random", help="submit N randomly chosen jobs",
+                        type=int, metavar='N')
+
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
+        print("invoke with --help to see usage information.")
+        sys.exit(1)
+    if args.completed:
+        completed = show_completed()
+        for item in completed:
+            print(item)
+    elif args.in_progress:
+        in_progress = show_in_progress()
+        for item in in_progress:
+            print(item)
+    elif args.submit_small:
+        result = submit_small(args.submit_small)
+        print(json.dumps(result, sort_keys=True, indent=4))
+    elif args.submit_random:
+        result = submit_random(args.submit_random)
+        print(json.dumps(result, sort_keys=True, indent=4))
+
+if __name__ == "__main__":
+    main()
