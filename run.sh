18649ec8be3629498021b2e6c7ee12989a6203c2
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-04-17 16:08:14 -0700

    tweak more s3 settings

--- run.sh
+++ run.sh
@@ -21,6 +21,11 @@ echo container id is $container_id
 aws s3 cp $ACCESSION_LIST accessionlist.txt
 
 aws configure set default.s3.multipart_chunksize 50MB
+aws configure set default.s3.max_concurrent_requests 100
+aws configure set default.s3.max_queue_size 10000
+aws configure set default.s3.multipart_threshold 64MB
+
+
 
 
 if [[ -v AWS_BATCH_JOB_ID ]]
